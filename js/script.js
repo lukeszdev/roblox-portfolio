c3718e6dec7a887f759a94ab146f9b05110ce2e0
Lucas <90071220+lukeszdev@users.noreply.github.com>
2024-09-20 19:07:07 -0300

    implemented slider for project cards

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".projects-cards", {
+    slidesPerView: 4,
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});

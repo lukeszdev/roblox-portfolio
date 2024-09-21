41ac0fed4b257dc6b5fdf99e59f159f585e95528
Lucas <90071220+lukeszdev@users.noreply.github.com>
2024-09-21 01:10:22 -0300

    created contact section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".projects-cards", {
-    slidesPerView: 4,
-    spaceBetween: 50,
+    slidesPerView: 3,
+    spaceBetween: 20,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
